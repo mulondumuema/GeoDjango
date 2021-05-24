8c14f40969efe3b8767ab23dd39b5a7bffb9163a
mulondu <mulondumuema@gmail.com>
2021-05-24 06:49:09 +0300

    update index.html

--- djangogis/counties/urls.py
+++ djangogis/counties/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, county_points
+from .views import HomePageView, county_points, county_datasets
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('countypoints/', county_points, name="countypoints"),
+    path('countiesdata/', county_datasets, name="countiesdata"),
 ]
