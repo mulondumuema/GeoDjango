1276a4929e1932e7f97f31ee5744273bc022b985
julia <jmugure95@gmail.com>
2021-06-02 15:36:45 +0300

    fix  show the loaded county layers

--- djangogis/counties/models.py
+++ djangogis/counties/models.py
@@ -1,10 +1,12 @@
 from django.contrib.gis.db import models
 
+
 class County(models.Model):
     name = models.CharField(max_length=100)
-    location = models.PointField(srid=4326)   # PointField is aGeoDjango specificgeometricfieldforstoring a GEOS point
+    location = models.PointField(srid=4326)  # PointField is aGeoDjango specificgeometricfieldforstoring a GEOS point
     capital = models.CharField(max_length=50)
     population = models.IntegerField()
+
 
 class counties(models.Model):
     county_1 = models.CharField(max_length=25)
--- djangogis/counties/views.py
+++ djangogis/counties/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 from django.core.serializers import serialize
-from djgeojson.views import GeoJSONLayerView
+from django.http import HttpResponse
+# from djgeojson.views import GeoJSONLayerView
 from .models import County, counties
 
 
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name= 'index.html'
+    template_name = 'index.html'
+
 
 def county_points(request):
-	countypoints = serialize('geojson', County.objects.all())
-	return HttpResponse(countypoints,content_type='application/json')
+    countypoints = serialize('geojson', County.objects.all())
+    return HttpResponse(countypoints, content_type='application/json')
+
 
 def county_datasets(request):
-	countiesdata = serialize('geojson', counties.objects.all())
-	return HttpResponse(countiesdata,content_type='application/json')
+    countiesdata = serialize('geojson', counties.objects.all())
+    return HttpResponse(countiesdata, content_type='application/json')
